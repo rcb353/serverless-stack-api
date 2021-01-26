9ee6ce9613d2595981ebb3e0a27db661b420b630
rachel.baer <rachel.baer@gmail.com>
2021-01-26 15:46:26 -0800

    Adding serverless API

---
+++ libs/handler-lib.js
@@ -0,0 +1,22 @@
+export default function handler(lambda) {
+  return async function (event, context) {
+    let body, statusCode;
+
+    try {
+      // Run the Lambda
+      body = await lambda(event, context);
+      statusCode = 200;
+    } catch (e) {
+      //uncomment the line below to print out the full error
+      console.log(e);
+      body = { error: e.message };
+      statusCode = 500;
+    }
+
+    // Return HTTP response
+    return {
+      statusCode,
+      body: JSON.stringify(body),
+    };
+  };
+}
